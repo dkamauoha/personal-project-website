53086743cdd9980586bba3cb77901b7720c2d116
Dylan Kamauoha <dylantkamauoha@gmail.com>
2018-08-17 13:58:48 -0600

    Set PROTOCOL

--- server/index.js
+++ server/index.js
@@ -61,7 +61,7 @@ app.get('/auth/callback', async (req, res) => {
         client_secret: CLIENT_SECRET,
         code: req.query.code,
         grant_type: 'authorization_code',
-        redirect_uri: `http://${req.headers.host}/auth/callback`
+        redirect_uri: `${process.env.PROTOCOL}://${req.headers.host}/auth/callback`
     }
     let resWithToken = await axios.post(`https://${REACT_APP_DOMAIN}/oauth/token`, payload);
     let resWithUserData = await axios.get(`https://${REACT_APP_DOMAIN}/userinfo?access_token=${resWithToken.data.access_token}`);
