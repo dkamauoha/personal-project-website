b8af17af0b7383399bf6e1f29dffc2d6173385ef
Dylan Kamauoha <dylantkamauoha@gmail.com>
2018-08-19 14:18:10 -0600

    Mostly styled  Services component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,7 @@ export default function About () {
         <div style={{height: '90vh', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', fontFamily: 'Montserrat, serif'}}>
             <h2 className='about__title'>Amber Kamauoha</h2>
             <img src='http://www.ambershairandlashes.com/_/rsrc/1506869912282/contact-us/DSC_0344%20%283%29.JPG?height=320&width=212' alt=''/>
-            <h4>Independent Stylist</h4>
+            <h4>Hair Stylist and Eyelash Technician</h4>
         </div>
     )
 }
